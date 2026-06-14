cc4d08451842074cec3470c1352aa57685aabcfd
rianpuc <redlopes@sga.pucminas.br>
2026-06-14 20:45:19 -0300

    chore: returning old models to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ContatoList from './components/ContatoList';
+import ContatoForm from './components/ContatoForm';
+import CompromissoList from './components/CompromissoList';
+import CompromissoForm from './components/CompromissoForm';
 import AtendimentoList from './components/AtendimentoList';
 import AtendimentoForm from './components/AtendimentoForm';
 import './App.css';
@@ -11,11 +15,20 @@ function App() {
         <nav className="navbar">
           <h1>📅 Atendimentos Web</h1>
           <div className="nav-links">
+            <Link to="/contatos">Contatos</Link>
+            <Link to="/compromissos">Compromissos</Link>
             <Link to="/atendimentos">Atendimentos</Link>
           </div>
         </nav>
         <main className="container">
           <Routes>
+            <Route path="/" element={<ContatoList />} />
+            <Route path="/contatos" element={<ContatoList />} />
+            <Route path="/contatos/novo" element={<ContatoForm />} />
+            <Route path="/contatos/editar/:id" element={<ContatoForm />} />
+            <Route path="/compromissos" element={<CompromissoList />} />
+            <Route path="/compromissos/novo" element={<CompromissoForm />} />
+            <Route path="/compromissos/editar/:id" element={<CompromissoForm />} />
             <Route path="/atendimentos" element={<AtendimentoList />} />
             <Route path="/atendimentos/novo" element={<AtendimentoForm />} />
             <Route path="/atendimentos/editar/:id" element={<AtendimentoForm />} />
